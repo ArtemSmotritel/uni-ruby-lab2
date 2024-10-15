4b856c6c9e1f22abe6632ba081740ebd477f2ff8
Artem Smotrytel <artemsmotritel@gmail.com>
2024-10-15 22:52:48 +0300

    run linter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "abobus", password: "abobus", except: [:index, :show]
+  http_basic_authenticate_with name: "abobus", password: "abobus", except: [ :index, :show ]
 
   def index
     @articles = Article.all
